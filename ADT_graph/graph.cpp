842226aa89ff2bb719a248d2e34d06c8735bb664
monty-svg <shiming727@gmail.com>
2023-04-08 22:17:58 +0800

    add graph class

--- ADT_graph/graph.cpp
+++ ADT_graph/graph.cpp
@@ -4,5 +4,15 @@
 //
 //  Created by Monty shi on 2023/4/8.
 //
-
+#ifndef _GRAPH_HXX_
+#define _GRAPH_HXX_
 #include "graph.hpp"
+
+
+class Graph{
+public:
+    Graph();
+    ~Graph();
+};
+
+#endif
