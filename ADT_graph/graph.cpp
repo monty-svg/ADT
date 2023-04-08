6d280e69b71ac93cbdc345f0eeaa48cf3a1a765f
monty-svg <shiming727@gmail.com>
2023-04-08 17:51:48 +0800

    add graph data struct file

---
+++ ADT_graph/graph.cpp
@@ -0,0 +1,8 @@
+//
+//  graph.cpp
+//  ADT_graph
+//
+//  Created by Monty shi on 2023/4/8.
+//
+
+#include "graph.hpp"
---
+++ ADT_graph/graph.hpp
@@ -0,0 +1,13 @@
+//
+//  graph.hpp
+//  ADT_graph
+//
+//  Created by Monty shi on 2023/4/8.
+//
+
+#ifndef graph_hpp
+#define graph_hpp
+
+#include <stdio.h>
+
+#endif /* graph_hpp */
